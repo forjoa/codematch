ed285bd4a50e9d7e6827b00345111593ec695b91
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-11-10 13:22:41 +0100

    Fix: Register form finally working

--- src/components/Register/RegisterForm.jsx
+++ src/components/Register/RegisterForm.jsx
@@ -39,7 +39,7 @@ const RegisterForm = () => {
     }, [])
 
     const handleSubmit = async (e) => {
-        e.preventDefault
+        e.preventDefault()
 
         let languages = selectedLanguages.join('/')
 
